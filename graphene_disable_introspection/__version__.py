8ba06b29325ac25578f2d2f868ef4ddf091cffdf
Carlo Völker <c.voelker@s-v.de>
2024-08-13 21:40:37 +0200

    changes on setup.py

--- graphene_disable_introspection/__version__.py
+++ graphene_disable_introspection/__version__.py
@@ -1,7 +1,7 @@
-__title__ = 'graphene-disable-introspection'
-__description__ = 'graphene-disable-introspection - Middleware for Python Graphene to disable introspection'
-__version__ = '0.1'
-__author__ = 'Carlo Völker'
-__author_email__ = 'carlovoelker@gmx.net'
-__license__ = 'GPL-3.0'
-__url__ = 'https://github.com/Paprikaschote/graphene-disable-introspection'
+__title__ = "graphene_disable_introspection"
+__description__ = "graphene-disable-introspection - Middleware for Python Graphene to disable introspection"
+__version__ = "0.1"
+__author__ = "Carlo Völker"
+__author_email__ = "carlovoelker@gmx.net"
+__license__ = "GPL-3.0"
+__url__ = "https://github.com/Paprikaschote/graphene-disable-introspection"
