7de045c285be6d56cf490010d2a67a65f833ffff
Carlo Völker <carlovoelker@gmx.net>
2024-09-19 12:22:28 +0200

    fixing middleware code

--- graphene_disable_introspection/middleware.py
+++ graphene_disable_introspection/middleware.py
@@ -4,5 +4,5 @@ from graphene_disable_introspection.conf import settings
 class DisableIntrospectionMiddleware:
     def resolve(self, next, root, info, **args):
         if info.field_name in settings.DISABLED_INTROSPECTION_TYPES:
-            raise Exception("Introspection is disabled")
+            return "[disabled]"
         return next(root, info, **args)
